87093539e6ff566e330ce725aed8bf96179be650
Ayushi150104 <choudharybinaykumar0@gmail.com>
2026-03-10 17:15:44 +0530

    All Done

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000"; // Fixed leading space
+  const url = "https://food-del-bz74.onrender.com"; // Fixed leading space
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
