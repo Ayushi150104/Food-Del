f3d58b315689adaa2f0684ce5fb652d31ab4f32c
Ayushi150104 <choudharybinaykumar0@gmail.com>
2026-03-10 11:35:54 +0530

    Made a Order Modal

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 export const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000"; //"https://food-del-bz74.onrender.com"
+  const url = "https://food-del-bz74.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
